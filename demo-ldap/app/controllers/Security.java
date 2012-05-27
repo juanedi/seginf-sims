c990cddc9e9e0217ea7b94f05cc6cbaa3782fc11
Juan Edi <juanedi@zauberlabs.com>
2012-05-26 22:22:10 -0300

    Agrego app para demo LDAP

---
+++ demo-ldap/app/controllers/Security.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2012 Zauber S.A.  -- All rights reserved
+ */
+package controllers;
+
+
+/**
+ * Autentica contra el LDAP
+ * 
+ * 
+ * @author Juan Edi
+ * @since May 26, 2012
+ */
+public class Security extends controllers.Secure.Security {
+
+    static boolean authenticate(final String username, final String password) {
+        return false;
+    }
+    
+}
