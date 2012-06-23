67144d30b72b5e7f6e19390a6d37d32ea43747e3
Ruben Festini <rfestini@rfestini.(none)>
2012-06-23 20:43:16 -0300

    Pantalla inicial de cambio de clave "evolucionara"

--- sims/app/controllers/Password.java
+++ sims/app/controllers/Password.java
@@ -3,6 +3,10 @@ package controllers;
 import play.mvc.Controller;
 import play.mvc.With;
 
+import models.User;
+import play.data.validation.Required;
+import play.mvc.Controller;
+
 /**
  * Controller de pantalla de cambio de passwords.
  * 
@@ -17,4 +21,33 @@ public class Password extends SecureController {
         render();
     }
     
+    
+    public static void changePassword(	@Required final String password, 
+            							@Required final String newPassword, 
+            							@Required final String newPasswordConfirmation) {
+    		
+    	
+    							User user = connectedUser();
+
+    							if (validation.hasErrors()) {
+    								flash.error("Todos los campos son obligatorios");
+    								info();
+    							}
+    							
+    							if (!newPassword.equals(newPasswordConfirmation)) {
+    								flash.error("Los passwords no coinciden.");
+    								info();            
+    							}
+    							if (!user.comparePassword(password)) {
+    								flash.error("La password ingresada no es correcta.");
+    								info();            
+    							}
+    							
+    							user.setPassword(newPasswordConfirmation);
+    							user.save();
+
+    							flash.success("Se ha actualizado su clave");
+    							Application.index();
+    }
+
 }
