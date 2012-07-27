71fba0238f4e34dd15d58eddd9a8f51f12be2e88
Juan Edi <juanedi@zauberlabs.com>
2012-07-26 23:12:32 -0300

    agrego al modelo eventos de auditoría

---
+++ sims/app/models/Event.java
@@ -0,0 +1,62 @@
+package models;
+
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+
+import org.apache.commons.lang.Validate;
+
+import play.db.jpa.Model;
+
+/**
+ * Evento de auditoría.
+ * 
+ * @author Juan Edi
+ */
+public class Event extends Model {
+
+	@Column(name = "type")
+	@Enumerated(EnumType.STRING)
+	public EventType type;
+	
+	@Column(name = "date", nullable = false)
+	public Date date;
+	
+    @ManyToMany
+    @JoinTable( name = "events_apps", 
+                joinColumns = @JoinColumn(name = "event_id"), 
+                inverseJoinColumns = @JoinColumn(name = "app_id"))
+	public List<App> relatedApps;
+
+    @ManyToMany
+    @JoinTable( name = "users_apps", 
+                joinColumns = @JoinColumn(name = "user_id"), 
+                inverseJoinColumns = @JoinColumn(name = "app_id"))
+	public List<User> relatedUsers;
+	
+    @Column(name = "description", nullable = false)
+    public String description;
+
+    /** constructor default */
+    public Event() {
+    	// Hibernate
+    }
+    
+    /** construye un evento nuevo */
+	public Event(final List<App> relatedApps, final List<User> relatedUsers, String description) {
+		Validate.notNull(relatedApps);
+		Validate.notNull(relatedUsers);
+		Validate.notEmpty(description);
+		this.date = new Date();
+		this.relatedApps = relatedApps;
+		this.relatedUsers = relatedUsers;
+		this.description = description;
+	}
+	
+}
---
+++ sims/app/models/EventType.java
@@ -0,0 +1,16 @@
+package models;
+
+/**
+ * Tipos de evento de auditoría
+ * 
+ * @author Juan Edi
+ *
+ */
+public enum EventType {
+
+	PASSWORD_CHANGE,
+	USER_CREATED,
+	APP_CREATED,
+	PASSWORD_POLICY_CHANGE;
+	
+}
